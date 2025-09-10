1498a8adbe7df40392160775840cb919718e3909
djfairbanks-utep <dallinfair96@gmail.com>
2025-09-09 21:44:16 -0600

    cleaning via flake8 linter

--- assignment.py
+++ assignment.py
@@ -1,13 +1,15 @@
 import random
-#Do not remove the parameter from the main function
+# Do not remove the parameter from the main function
+
+
 def main(random_number=None):
-    #Do not remove line 4, 5, or 6
+    # Do not remove line 4, 5, or 6
     if random_number is None:
-        random_number = random.randint(1,10)
-    
-    #Your code starts here.
-    #Remember your code needs to be outside the if statement
-    
+        random_number = random.randint(1, 10)
+
+    # Your code starts here.
+    # Remember your code needs to be outside the if statement
+
 
 if __name__ == "__main__":
     main()
