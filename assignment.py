d4b34fe1e635dc8748c0772cfd483c27183ee079
Richard Hernandez <rbhernandez5@miners.utep.edu>
2025-10-26 23:44:19 -0600

    Attempt 1

--- assignment.py
+++ assignment.py
@@ -7,9 +7,22 @@ def main(random_number=None):
     if random_number is None:
         random_number = random.randint(1, 10)
 
-    # Your code starts here.
-    # Remember your code needs to be outside the if statement
+    print("\nWelcome to the Guessing Game")
 
+   
+    for i in range(3):
+       guess_number = int(input("\nEnter your guess from one to ten: "))
+    
+       if guess_number == random_number:
+         print(f"Hooray! You guessed the number: {guess_number}")
+    
+       elif guess_number > random_number:
+         print(f"Your guess {guess_number} was a little too high. Try lower.")
+
+       else:
+         print(f"Your guess {guess_number} was a little too low. Try higher.")
+    
+    print(f"You failed to guess the number {random_number} in three attempts.")
 
 if __name__ == "__main__":
     main()
